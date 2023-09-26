0359c900925c830ae866e44f681bdd747e6ab109
Eiint <145400279+eiint@users.noreply.github.com>
2023-09-26 13:53:20 +0300

    Update String.py

--- String.py
+++ String.py
@@ -1,16 +1,16 @@
-import pyrogram 
- 
-app = pyrogram.Client( 
-    "session_name", 
-    20037631, 
-    "ee6caffb104ca333abff9c1d8b90b229", 
-    in_memory=True 
-) 
- 
-app.start() 
- 
-string_session = app.export_session_string() 
- 
-app.send_message("me", f"{string_session}") 
- 
+import pyrogram
+
+app = pyrogram.Client(
+    "session_name",
+    13251350,
+    "66c0eacb36f9979ae6d153f207565cd6",
+    in_memory=True
+)
+
+app.start()
+
+string_session = app.export_session_string()
+
+app.send_message("me", f"`{string_session}`")
+
 print("\n\nCHECK SAVED MESSAGES")
